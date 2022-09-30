c3bb463e17b320c8b4ab2078e8ac4311c7cd284f
HIMOONGOD <105285685+HIMOONGOD@users.noreply.github.com>
2022-09-30 16:04:17 +0630

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,17 +1,37 @@
-
 const startball = setTimeout(() => {
   document.querySelector(".moon").classList.add("moon2");
-},1000)
+}, 1000);
 
 setTimeout(() => {
-    document.querySelector(".moon").classList.remove("moon2");
-  },10000)
+  document.querySelector(".moon").classList.remove("moon2");
+}, 10000);
 
-setInterval(( ) => {
-    setTimeout(() => {
-        document.querySelector(".moon").classList.add("moon2");
-      },1000)
-      setTimeout(() => {
-          document.querySelector(".moon").classList.remove("moon2");
-        },  10000)
-},14000)
+setInterval(() => {
+  setTimeout(() => {
+    document.querySelector(".moon").classList.add("moon2");
+  }, 1000);
+  setTimeout(() => {
+    document.querySelector(".moon").classList.remove("moon2");
+  }, 10000);
+}, 14000);
+
+// For auto Typing
+
+try {
+  const TextArray = ["Myo Lamin Maung", "Newbie Coder","Beginner web designer"];
+  const AutoTag = document.querySelector(".autoType");
+  AutoTag.innerHTML = TextArray[0];
+  let i = 1;
+  setInterval(() => { 
+    if (i < 3) {
+       AutoTag.innerHTML = TextArray[i];
+       i++;
+    } else {
+      
+      i = 0;
+    }
+   
+  },3000);
+} catch (error) {
+  console.log("err");
+}
