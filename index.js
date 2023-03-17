a00247a86e704bca5229feafdd9681e2c9edb7dc
HIMOONGOD <105285685+HIMOONGOD@users.noreply.github.com>
2023-03-17 10:40:07 +0630

    N-0.1

---
+++ index.js
@@ -0,0 +1,25 @@
+const project = [
+    {
+        projectName : "note",
+        projectURL :"",
+        projectDate:"",
+    },
+    {
+        projectName : "Coffee",
+        projectURL :"",
+        projectDate:"",
+    },
+    {
+        projectName : "note",
+        projectURL :"",
+        projectDate:"",
+    },
+    {
+        projectName : "note",
+        projectURL :"",
+        projectDate:"",
+    },
+
+]
+
+console.log(project[0].projectName)
