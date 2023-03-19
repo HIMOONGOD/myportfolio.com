ba1817043a994ad8fe17da7e8c4e6da0e07d612f
HIMOONGOD <105285685+HIMOONGOD@users.noreply.github.com>
2023-03-19 21:20:40 +0630

    n-0.2

--- index.js
+++ index.js
@@ -1,25 +1,43 @@
 const project = [
     {
         projectName : "note",
-        projectURL :"",
+        projectURL :"project/Array note/index.html",
         projectDate:"",
+        projectPhoto:"url('src/note.png')",
     },
     {
         projectName : "Coffee",
-        projectURL :"",
+        projectURL :"project/coffeeShop/Ui1.html",
         projectDate:"",
+        projectPhoto:"url('src/coffee.jpg')",
     },
     {
-        projectName : "note",
-        projectURL :"",
+        projectName : "nothing",
+        projectURL :"project/Formation PJ/index.html",
         projectDate:"",
-    },
-    {
-        projectName : "note",
-        projectURL :"",
-        projectDate:"",
-    },
+        projectPhoto:"",
+    }
 
 ]
 
-console.log(project[0].projectName)
+console.log(project.length)
+
+// for building card
+const projectBoxTag = document.getElementById("projectRoom")
+
+for (let index = 0; index < project.length; index++) {
+    const projectCardTag = document.createElement("div");
+    projectCardTag.classList.add("projectCard");
+    projectCardTag.style.backgroundImage =  project[index].projectPhoto;
+    projectBoxTag.append(projectCardTag);
+    projectCardTag.innerText = project[index].projectName;
+
+    // for go project
+
+    projectCardTag.addEventListener("click",() => {
+        console.log("work");
+        location = project[index].projectURL;
+    })
+    
+    
+}
